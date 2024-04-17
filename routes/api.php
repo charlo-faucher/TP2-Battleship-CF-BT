5092b87f7184eb71fe89722155bbf6c6bb07a8ce
BenjaminT02 <1807842@cstjean.qc.ca>
2024-04-17 13:49:59 -0400

    premier commit

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('partie')
+    ->controller(PartieController::class)
+    ->group(function () {
+        Route::get('/{id}', 'spect');
+        Route::post('/', 'index');
+        Route::post('/{id}/missile', 'aifire');
+        Route::post('/{id}/missile/{coordonée}', 'fire');
+        Route::delete('/{id}', 'destroy');
+    });
