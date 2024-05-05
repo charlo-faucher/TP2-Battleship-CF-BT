afc666b48245babd980d6f1683e464136f98e15f
charlo-faucher <fauchercharlo@gmail.com>
2024-05-05 14:32:44 -0400

    2 defensive

--- app/Algorithmes/DefensiveBattleship.php
+++ app/Algorithmes/DefensiveBattleship.php
@@ -8,6 +8,9 @@ use App\Models\CoordonneeBateauOrdinateur;
 use App\Models\Partie;
 use App\Models\TypeBateau;
 
+/**
+ * Classe pour la défense des bateaux (placement des bateaux).
+ */
 class DefensiveBattleship
 {
     /**
@@ -15,6 +18,8 @@ class DefensiveBattleship
      *
      * @param Partie $partie - Partie à débuter
      * @return void
+     *
+     * @author Charles-Olivier Faucher & Benjamin Theriault
      */
     public static function creerBateaux(Partie $partie) : void
     {
@@ -34,12 +39,76 @@ class DefensiveBattleship
             $bateaux[$bateauModele->type->nom] = ['id' => $bateau->id, 'taille' => $bateauModele->type->taille, 'bateau' => $bateauModele];
         }
 
-        DefensiveBattleship::strategieEcart($bateaux);
+        switch (rand(1, 2))
+        {
+            case 1:
+                DefensiveBattleship::strategieBordures($bateaux);
+                break;
+            case 2:
+                DefensiveBattleship::strategieEcart($bateaux);
+        }
+    }
+
+    /**
+     * Stratégie de placement des bateaux qui place les bateaux aux bordures.
+     *
+     * Exemple :
+     *    1 2 3 4 5 6 7 8 9 10
+     *  A X X X X X - - - X -
+     *  B - - - - - - - - X -
+     *  C - - - - - - - - X -
+     *  D - - - - - - - - - -
+     *  E X X - - - - - - - -
+     *  F - - - - - - - - - -
+     *  G - - - - - - - - - X
+     *  H - - - - - - - - - X
+     *  I - - - - - - - - - X
+     *  J X X X X - - - - - -
+     *
+     * @param array $bateaux - Bateaux de la partie
+     * @return void
+     */
+    private static function strategieBordures(array $bateaux) : void
+    {
+        $bateauxGauche = [$bateaux['patrouilleur'], $bateaux['porte-avions'], $bateaux['cuirasse']];
+        $positionsGauche = [['A', 'B'], ['E', 'F'], ['I', 'K']];
+
+        shuffle($bateauxGauche);
+
+        foreach ($bateauxGauche as $bateau) {
+            $positionArr = array_pop($positionsGauche);
+            $position = $positionArr[array_rand($positionArr)];
+            for ($i = 1; $i <= $bateau['taille']; $i++) {
+                CoordonneeBateauOrdinateur::create([
+                    'coordonnee' => $position.'-'.$i,
+                    'bateau_id'=> $bateau['id']
+                ]);
+            }
+        }
+
+        $bateauxDroite = [$bateaux['sous-marin'], $bateaux['destroyer']];
+        $positionsVerticalesDroite = [['A', 'B'], ['G', 'H']];
+        $positionsHorizontalesDroite = [9, 10];
+
+        shuffle($bateauxDroite);
+
+        foreach ($bateauxDroite as $bateau) {
+            $positionVertiArr = array_pop($positionsVerticalesDroite);
+            $positionVerti = ord($positionVertiArr[array_rand($positionVertiArr)]);
+            $positionHorizon = $positionsHorizontalesDroite[array_rand($positionsHorizontalesDroite)];
+            for ($i = 0; $i < $bateau['taille']; $i++) {
+                CoordonneeBateauOrdinateur::create([
+                    'coordonnee' => chr($positionVerti + $i).'-'.$positionHorizon,
+                    'bateau_id'=> $bateau['id']
+                ]);
+            }
+        }
     }
 
     /**
      * Stratégie de placement des bateaux qui place les bateaux dans leurs "rangée" respective.
      *
+     * Exemple :
      *    1 2 3 4 5 6 7 8 9 10
      *  A X X X X X - - - - -
      *  B - - X - - - - - - -
